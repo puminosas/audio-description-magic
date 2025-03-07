deb0f3c3116dd87bda216c074c80b3766b6f53a4
puminosas <puminosas@gmail.com>
2025-03-07 04:47:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: audiodescriptions.online
     port: 8080,
   },
   preview: {
